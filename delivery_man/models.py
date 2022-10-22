28be2b41a02d492085e7942ba451c882d99cbf9d
Diego Alexander Moreno Cordoba <damc@hotmail.es>
2022-10-22 18:59:01 -0500

    added DeliveryMan model

--- delivery_man/models.py
+++ delivery_man/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class DeliveryMan(models.Model):
+    lat = models.IntegerField()
+    long = models.IntegerField()
+    last_update = models.DateTimeField()
