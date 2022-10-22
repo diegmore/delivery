92ab8583ddcca175165878766aac5541aa9f5308
Diego Alexander Moreno Cordoba <damc@hotmail.es>
2022-10-22 18:59:41 -0500

    added delivery man on installed apps

--- delivery/settings.py
+++ delivery/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'orders.apps.OrdersConfig',
+    'delivery_man.apps.DeliveryManConfig',
 ]
 
 MIDDLEWARE = [
