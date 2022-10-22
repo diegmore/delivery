52541a4c43bbb96551a6f37de49d3ab17050917e
Diego Alexander Moreno Cordoba <damc@hotmail.es>
2022-10-22 18:52:17 -0500

    created order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    delivery_date = models.DateTimeField()
+    delivery_lat = models.IntegerField()
+    delivery_long = models.IntegerField()
+    destination_lat = models.IntegerField()
+    destination_long = models.IntegerField()
+    
