0d67136e29c9ea10a3cb6b17cc7a6f8bc5879d22
Diego Alexander Moreno Cordoba <damc@hotmail.es>
2022-10-22 20:56:46 -0500

    added validation on create order serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Order
+from datetime import timedelta
+
 
 class OrderSerializer(serializers.ModelSerializer):
+    
+    def validate(self, attrs):
+        added_datetime = attrs.get('delivery_date') + timedelta(hours=1)
+        if Order.objects.filter(delivery_man = attrs.get('delivery_man').id, 
+                                delivery_date__gte=attrs.get('delivery_date'),
+                                delivery_date__lt=added_datetime):
+            raise serializers.ValidationError('This delivery man already has a order at this time')
+        return attrs
     
     class Meta:
         model = Order
