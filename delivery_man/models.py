fda8a62b5a5fa6e0b3963e30c3e8f25576b7a57e
Diego Alexander Moreno Cordoba <damc@hotmail.es>
2022-10-22 19:17:55 -0500

    added default values on models

--- delivery_man/models.py
+++ delivery_man/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class DeliveryMan(models.Model):
-    lat = models.IntegerField()
-    long = models.IntegerField()
-    last_update = models.DateTimeField()
+    lat = models.IntegerField(default=0)
+    long = models.IntegerField(default=0)
+    last_update = models.DateTimeField(auto_now_add=True)
