866e99247a7e12e890018c93250ddb99c5689c4f
Diego Alexander Moreno Cordoba <damc@hotmail.es>
2022-10-22 21:30:06 -0500

    added all tests from 4 endpoints

--- orders/tests.py
+++ orders/tests.py
@@ -37,10 +37,68 @@ class CompanyTests(APITestCase):
             "destination_lat": 50,
             "destination_long": 50
         }
-        Order.objects.create(**data, delivery_man=DeliveryMan.objects.all().last())
+        Order.objects.create(
+            **data, delivery_man=DeliveryMan.objects.all().last())
         data.update({'delivery_man': DeliveryMan.objects.all().last().id})
         # when
         url = reverse('order-list')
         response = self.client.post(url, data, format='json')
         # then
-        self.assertEqual(response.json(), {'non_field_errors': ['This delivery man already has a order at this time']})
+        self.assertEqual(response.json(), {'non_field_errors': [
+                         'This delivery man already has a order at this time']})
+
+    def test_get_all_orders(self):
+        # given
+        data = {
+            "delivery_date": "2022-10-22T20:05:00.000Z",
+            "delivery_lat": 1,
+            "delivery_long": 1,
+            "destination_lat": 50,
+            "destination_long": 50
+        }
+        Order.objects.create(
+            **data, delivery_man=DeliveryMan.objects.all().last())
+        # when
+        url = reverse('order-list')
+        response = self.client.get(url, {'orderedDate': '2022-10-22'})
+        # then
+        self.assertEqual(response.json(), [{'id': 3, 'delivery_man': 3, 'delivery_date': '2022-10-22T20:05:00Z',
+                         'delivery_lat': 1, 'delivery_long': 1, 'destination_lat': 50, 'destination_long': 50}])
+
+    def test_get_all_orders_by_delivery_man(self):
+        # given
+        data = {
+            "delivery_date": "2022-10-22T20:05:00.000Z",
+            "delivery_lat": 1,
+            "delivery_long": 1,
+            "destination_lat": 50,
+            "destination_long": 50
+        }
+        Order.objects.create(
+            **data, delivery_man=DeliveryMan.objects.all().last())
+        # when
+        url = reverse('order-get-orders-by-deliveryman')
+        response = self.client.get(
+            url, {'orderedDate': '2022-10-22', 'deliveryMan': 4})
+        # then
+        self.assertEqual(response.json(), [{'id': 4, 'delivery_man': 4, 'delivery_date': '2022-10-22T20:05:00Z',
+                         'delivery_lat': 1, 'delivery_long': 1, 'destination_lat': 50, 'destination_long': 50}])
+
+    def test_get_nearest_delivery_man(self):
+        # given
+        data = {
+            "delivery_date": "2022-10-22T20:05:00.000Z",
+            "delivery_lat": 1,
+            "delivery_long": 1,
+            "destination_lat": 50,
+            "destination_long": 50
+        }
+        Order.objects.create(
+            **data, delivery_man=DeliveryMan.objects.all().last())
+        # when
+        url = reverse('order-get-near-deliveryman')
+        response = self.client.get(
+            url, {'queryDate': '2022-10-22T20:05:00.000', 'queryLat': 51, 'queryLng': 51})
+        # then
+        self.assertEqual(response.json(), {
+                         'deliver_id': 5, 'actual_lat': 50, 'actual_lng': 50})
