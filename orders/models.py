d7ea5c8fa8fb6d7c2a73ff45522ac332fcebe7eb
Diego Alexander Moreno Cordoba <damc@hotmail.es>
2022-10-22 19:34:28 -0500

    added delivery man foreign key

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from delivery_man.models import DeliveryMan
 # Create your models here.
 class Order(models.Model):
+    delivery_man = models.ForeignKey(DeliveryMan, on_delete=models.CASCADE)
     delivery_date = models.DateTimeField()
     delivery_lat = models.IntegerField()
     delivery_long = models.IntegerField()
